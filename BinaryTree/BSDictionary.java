d1d2b5261fa8bb85bbbe5133ec81c5662e695ecb
BStross <150842183+BStross@users.noreply.github.com>
2023-11-27 15:47:53 -0800

    updated file header

--- BinaryTree/BSDictionary.java
+++ BinaryTree/BSDictionary.java
@@ -1,14 +1,3 @@
-// Darrell Criss
-// Class: CS145
-// 11/21/2023
-// References: https://www.java67.com/2016/08/binary-tree-inorder-traversal-in-java.html 
-// Bradford Stross
-// Assignment: Lab 6 - Binary Search Tree
-// Unsorted Binary Tree + basic operations
-
-// There are a few bugs, mostly in user input. 
-// Duplicate keys may also cause unintended behavior - you have been warned!
-
 package BinaryTree;
 
 import static BinaryTree.DManager.*;
